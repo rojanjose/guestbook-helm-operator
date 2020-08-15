0ae2cdce9ffaa5520d5a1e0be04a08effd8ed46a
rojanjose <rojanjose@gmail.com>
2020-08-14 21:59:35 -0400

    update install scripts

--- scripts/operatorInstall.sh
+++ scripts/operatorInstall.sh
@@ -1,41 +1,44 @@
 #!/bin/sh
 
-# Set the release version variable
-RELEASE_VERSION=v1.0.0
-# Linux
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu
+# First parameter should be the Operator version string, setting default value
+RELEASE_VERSION="${1:-v0.19.2}"
+#RELEASE_VERSION="${1:-v1.0.0}"
 
-# macOS
-echo "Downloading operaror-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "operaror-sdk for macOS downloaded."
-ls -al operator-sdk*
-echo "Installing operaror-sdk"
-chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+OS=${OS/darwin/apple-darwin}
+OS=${OS/linux/linux-gnu}
+ARCH=$(uname -m)
+FLAVOR=${ARCH}-${OS}
+
+echo -e "\nDownloading operaror-sdk-$RELEASE_VERSION-$FLAVOR ...\n"
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-${FLAVOR}
+echo -e "\noperaror-sdk-$RELEASE_VERSION-$FLAVOR downloaded.\n"
+ls -al operator-sdk-$RELEASE_VERSION-$FLAVOR
+echo -e "\nInstalling operaror-sdk-$RELEASE_VERSION-$FLAVOR\n"
+chmod +x operator-sdk-${RELEASE_VERSION}-${FLAVOR} && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-${FLAVOR} /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-${FLAVOR}
 
 
-echo "Downloading ansible-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "ansible-operator for macOS downloaded."
-ls -al ansible-operator*
-echo "Installing ansible-sdk"
-chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
+echo -e "\nDownloading ansible-sdk-$RELEASE_VERSION-$FLAVOR ...\n"
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/ansible-operator-${RELEASE_VERSION}-${FLAVOR}
+echo -e "\nansible-operator-$RELEASE_VERSION-$FLAVOR downloaded.\n"
+ls -al ansible-operator-$RELEASE_VERSION-$FLAVOR
+echo -e "\nInstalling ansible-sdk-$RELEASE_VERSION-$FLAVOR\n"
+chmod +x ansible-operator-${RELEASE_VERSION}-${FLAVOR} && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-${FLAVOR} /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-${FLAVOR}
 
-echo "Downloading helm-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "helm-sdk for macOS downloaded."
-ls -al helm-operator*
-echo "Installing helm-operator"
-chmod +x helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
+echo -e "\nDownloading helm-sdk-$RELEASE_VERSION-$FLAVOR ...\n"
+curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/helm-operator-${RELEASE_VERSION}-${FLAVOR}
+echo -e "\nhelm-sdk-$RELEASE_VERSION-$FLAVOR for macOS downloaded.\n"
+ls -al helm-operator-$RELEASE_VERSION-$FLAVOR
+echo -e "\nInstalling helm-operator-$RELEASE_VERSION-$FLAVOR\n"
+chmod +x helm-operator-${RELEASE_VERSION}-${FLAVOR} && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-${FLAVOR} /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-${FLAVOR}
 
-# Linux
-#chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-#chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-#chmod +x helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-
-# macOS
-#chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
-#chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-#chmod +x helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
+echo -e "Checking prereqs version ...\nGo version:"
+go version
+echo -e "-----------------------------\nHelm version:"
+helm version
+echo -e "-----------------------------\nOperator-sdk version:"
+operator-sdk version
+#echo -e "-----------------------------\nAnsible operator version:"
+#ansible-operator version
+#echo -e "-----------------------------\nHelm operator Version:"
+#helm-operator version
--- scripts/operatorUninstall.sh
+++ scripts/operatorUninstall.sh
@@ -1,41 +1,7 @@
 #!/bin/sh
 
-# Set the release version variable
-RELEASE_VERSION=v1.0.0
-# Linux
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-#curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-
-# macOS
-echo "Downloading operaror-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "operaror-sdk for macOS downloaded."
-ls -al operator-sdk*
-echo "Installing operaror-sdk"
-chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
-
-
-echo "Downloading ansible-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "ansible-operator for macOS downloaded."
-ls -al ansible-operator*
-echo "Installing ansible-sdk"
-chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-
-echo "Downloading helm-sdk for macOS..."
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-echo "helm-sdk for macOS downloaded."
-ls -al helm-operator*
-echo "Installing helm-operator"
-chmod +x helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-
-# Linux
-#chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
-#chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-#chmod +x helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-linux-gnu
-
-# macOS
-#chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/operator-sdk && rm operator-sdk-${RELEASE_VERSION}-x86_64-apple-darwin
-#chmod +x ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/ansible-operator && rm ansible-operator-${RELEASE_VERSION}-x86_64-apple-darwin
-#chmod +x helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin && sudo mkdir -p /usr/local/bin/ && sudo cp helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin /usr/local/bin/helm-operator && rm helm-operator-${RELEASE_VERSION}-x86_64-apple-darwin
+echo "Uninstalling operator-sdk ..."
+sudo rm /usr/local/bin/operator-sdk
+sudo rm /usr/local/bin/ansible-operator
+sudo rm /usr/local/bin/helm-operator
+echo "Uninstall complete."
